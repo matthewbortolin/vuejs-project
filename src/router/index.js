d231c7bd866d1650fe48161e8fef9a06410c7b16
Matt <matthewbortolin@hotmail.com>
2018-05-02 19:50:27 +1000

    import stylus, initialise and load data from firebase

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Signin from '@/components/Signin'
+import Update from '@/components/Update'
+import Practitioners from '@/components/Practitioners'
+import Practitioner from '@/components/Practitioner'
 
 Vue.use(Router)
 
@@ -8,8 +12,28 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/practitioners',
+      name: 'Practitioners',
+      component: Practitioners
+    },
+    {
+      path: '/practitioner',
+      name: 'Practitioner',
+      component: Practitioner
+    },
+    {
+      path: '/signin',
+      name: 'Signin',
+      component: Signin
+    },
+    {
+      path: '/update',
+      name: 'Update',
+      component: Update
     }
   ]
 })
