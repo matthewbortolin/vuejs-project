f549327033277f3bc77a0197b68328fbdb8ce300
Matt <matthewbortolin@hotmail.com>
2018-05-02 19:51:12 +1000

    import stylus, initialise and load data from firebase

--- src/main.js
+++ src/main.js
@@ -2,18 +2,37 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
 import Vuetify from 'vuetify'
+import * as firebase from 'firebase'
+import router from './router'
+import { store } from './store'
+
+import './stylus/main.styl'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
-
 Vue.config.productionTip = false
 
+// Initialise Firebase
+firebase.initializeApp({
+  apiKey: 'AIzaSyC8o9fGak_TQQFqsq5o_ZyhMWf-mk2CB0Y',
+  authDomain: 'fahf-2d5a0.firebaseapp.com',
+  databaseURL: 'https://fahf-2d5a0.firebaseio.com',
+  projectId: 'fahf-2d5a0',
+  storageBucket: 'fahf-2d5a0.appspot.com',
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  created () {
+    // load practitioners from firebase
+    this.$store.dispatch('loadPractitioners')
+
+    // load modalities from firebase
+    this.$store.dispatch('loadModalities')
+  },
 })
